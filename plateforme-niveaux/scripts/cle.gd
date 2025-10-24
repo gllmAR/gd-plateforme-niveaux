b252871eac9ace55a74a06c499024484e8e0afc1
Gllmar <gllmAR@users.noreply.github.com>
2025-10-24 09:57:40 -0400

    Fix audio stream resource definition and update parameter name in key script

--- plateforme-niveaux/scripts/cle.gd
+++ plateforme-niveaux/scripts/cle.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var groupe_cle:String
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	Main.debloquer_cle(self)
 	# Desactive collision
 	$CollisionShape2D.disabled = true
